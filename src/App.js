7a19f6519d38b6b234450886b1427786938e3b78
Gabe <jflores353@gmail.com>
2020-02-01 14:42:42 -0600

    passed props for test question from game to question. Able to display question with choices.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
--- src/Components/Question.js
+++ src/Components/Question.js
@@ -4,6 +4,12 @@ export default function Question({ question }) {
   return (
     <div>
       <h2>{question.question}</h2>
+      {question.answer.map((choice, index) => (
+        <div className="choice-container">
+          <p className="choice-prefix">{index + 1}</p>
+          <p className="choice-text">{choice}</p>
+        </div>
+      ))}
     </div>
   );
 }
