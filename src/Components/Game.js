6302577a43210ab9b4e788cb82c0e7ca229b3e53
Gabe <jflores353@gmail.com>
2020-02-01 22:16:35 -0600

    Created QuestionsHelper component and moved fetch into loadQuestions async function. Created separate function for converting the questions and use that in fetch to convert results. Also, previous commit removed questionTest object from game.js

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Question from "./Question";
+import { loadQuestions } from "../Helpers/QuestionsHelper";
 
 export default class Game extends Component {
   constructor(props) {
@@ -11,29 +12,8 @@ export default class Game extends Component {
   }
 
   async componentDidMount() {
-    const url = `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`;
     try {
-      const response = await fetch(url);
-      const { results } = await response.json();
-
-      const questions = results.map(loadedQuestion => {
-        const formattedQuestion = {
-          question: loadedQuestion.question,
-          answerChoices: [...loadedQuestion.incorrect_answers]
-        };
-
-        // This creates a random number to assign to the Correct answer to then splice into the answer choices array
-        formattedQuestion.answer = Math.floor(Math.random() * 4);
-
-        formattedQuestion.answerChoices.splice(
-          formattedQuestion.answer,
-          0,
-          loadedQuestion.correct_answer
-        );
-
-        return formattedQuestion;
-      });
-      // console.log(results);
+      const questions = await loadQuestions();
       console.log(questions);
       // Set state to values from fetch mapping, questions : questions same as { questions } since they are the same
       this.setState({ questions, currentQuestion: questions[0] });
---
+++ src/Helpers/QuestionsHelper.js
@@ -0,0 +1,30 @@
+export const loadQuestions = async () => {
+  const url = `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`;
+  try {
+    const response = await fetch(url);
+    const { results } = await response.json();
+    return convertQuestionsFromApi(results);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const convertQuestionsFromApi = rawQuestions => {
+  return rawQuestions.map(loadedQuestion => {
+    const formattedQuestion = {
+      question: loadedQuestion.question,
+      answerChoices: [...loadedQuestion.incorrect_answers]
+    };
+
+    // This creates a random number to assign to the Correct answer to then splice into the answer choices array
+    formattedQuestion.answer = Math.floor(Math.random() * 4);
+
+    formattedQuestion.answerChoices.splice(
+      formattedQuestion.answer,
+      0,
+      loadedQuestion.correct_answer
+    );
+
+    return formattedQuestion;
+  });
+};
