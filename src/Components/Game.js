f36e52ca720fab17016e8ac3e37116c5bf2efa5b
Gabe <jflores353@gmail.com>
2020-02-01 21:49:03 -0600

    created answer with random value. Spliced into our formatttedQuestion object to include the randomized answer value into the answerChoices array.

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -13,15 +13,25 @@ export default class Game extends Component {
       const response = await fetch(url);
       const { results } = await response.json();
 
-      const question = results.map(loadedQuestion => {
+      const questions = results.map(loadedQuestion => {
         const formattedQuestion = {
           question: loadedQuestion.question,
           answerChoices: [...loadedQuestion.incorrect_answers]
         };
+
+        // This creates a random number to assign to the Correct answer to then splice into the answer choices array
+        formattedQuestion.answer = Math.floor(Math.random() * 4);
+
+        formattedQuestion.answerChoices.splice(
+          formattedQuestion.answer,
+          0,
+          loadedQuestion.correct_answer
+        );
+
         return formattedQuestion;
       });
       console.log(results);
-      console.log(question);
+      console.log(questions);
     } catch (err) {
       console.error(err);
     }
