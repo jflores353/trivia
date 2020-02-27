4b72e27e1366c2bdc46d457aeee787a95a1b7bbc
Gabe <jflores353@gmail.com>
2020-02-27 01:06:06 -0600

    created config for Firebase with .env variables for DB. Had to add actual URL for DB in order to work in the browser. Made Firebase class to initialize the app and added scores( ) to become another key inside the DB entry.

--- src/Components/Firebase/Firebase.js
+++ src/Components/Firebase/Firebase.js
@@ -4,12 +4,13 @@ import 'firebase/database';
 const config = {
 	apiKey: process.env.REACT_APP_API_KEY,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	databaseURL: 'https://react-trivia-ba048.firebaseio.com',
 	projectID: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID
 };
 
-export default class Firebase {
+class Firebase {
 	constructor() {
 		app.initializeApp(config);
 		this.db = app.database();
@@ -17,3 +18,5 @@ export default class Firebase {
 
 	scores = () => this.db.ref('scores');
 }
+
+export default Firebase;
