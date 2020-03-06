2826af663089b160cfe246d679fe34398be7840a
Gabe <jflores353@gmail.com>
2020-03-06 14:12:04 -0600

    called firebase context inside useEffect to retrieve all scores

--- src/Components/HighScores.js
+++ src/Components/HighScores.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useFirebase } from './Firebase/FirebaseContext';
 
 export default function HighScores() {
+	const firebase = useFirebase();
+
+	useEffect(() => {
+		firebase.scores().once('value', snapshot => {
+			const data = snapshot.val();
+			console.log(data);
+		});
+	});
+
 	return (
 		<>
 			<h1>High Scores</h1>
