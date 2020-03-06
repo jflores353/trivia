3395b0d86f807454a4ba72f772055929879750fe
Gabe <jflores353@gmail.com>
2020-03-06 14:34:14 -0600

    Formatted the firebase scores by sorting them from highest to lowest scores. Then, called that function in useEffect sortedScores = formatScoreData(data)

--- src/Components/HighScores.js
+++ src/Components/HighScores.js
@@ -7,9 +7,27 @@ export default function HighScores() {
 	useEffect(() => {
 		firebase.scores().once('value', snapshot => {
 			const data = snapshot.val();
-			console.log(data);
+			const sortedScores = formatScoreData(data);
+			console.log(sortedScores);
 		});
 	});
+
+	const formatScoreData = firebaseScores => {
+		const scores = [];
+
+		//* The firebaseScore(s) have a key generated outside each object,
+		//* This loop assigns that key to a new key that we then push inside the object and can now have all
+		//* key values inside a more user friendly layout
+		for (let key in firebaseScores) {
+			const val = firebaseScores[key];
+			val['key'] = key;
+			scores.push(val);
+		}
+
+		return scores
+			.sort((score1, score2) => score2.score - score1.score)
+			.slice(0, 10);
+	};
 
 	return (
 		<>
