1eac2d7fb995f3cfe700da97050aac2b8d12b40a
Gabe <jflores353@gmail.com>
2020-03-30 13:44:03 -0500

    Added className for H1.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Home() {
 	return (
 		<>
-			<h1>Quiz App</h1>
+			<div className='title'>
+				<h1>React Quiz App</h1>
+			</div>
 			<Link to='/game' className='btn'>
 				Start Game
 			</Link>
